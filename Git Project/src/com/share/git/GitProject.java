6c30c58987b3e3a12e49060491975701f901b010
anmolsanap <anmolsan1996@gmail.com>
2024-05-14 17:37:30 +0530

    Project is shared

---
+++ Git Project/src/com/share/git/GitProject.java
@@ -0,0 +1,12 @@
+package com.share.git;
+
+public class GitProject {
+
+	public GitProject()
+	{
+		System.out.println("Constructor called");
+	}
+	public static void main(String[] args) {
+		System.out.println("main method started");
+	}
+}
